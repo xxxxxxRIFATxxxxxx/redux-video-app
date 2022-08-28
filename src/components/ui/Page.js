5ebc0e49be28dc14ad62c9a0aa402041995da6e4
Md. Rifat Islam <xxxxrifatxxxx@gmail.com>
2022-08-28 19:38:39 +0600

    Pagination Start

---
+++ src/components/ui/Page.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Page = ({number}) => {
+    return (
+        <div className="bg-blue-600 text-white px-4 py-1 rounded-full">{number}</div>
+    );
+};
+
+export default Page;
--- src/components/ui/Pagination.js
+++ src/components/ui/Pagination.js
@@ -1,19 +1,27 @@
+import { useSelector } from "react-redux";
+import Page from "./Page";
+
 export default function Pagination() {
+    const { videos } = useSelector(
+        (state) => state.videos
+    );
+
+    const pages = Math.ceil(videos.length / 2);
+
+    let content = [];
+
+    for (let i = 1; i <= pages; i++) {
+        content.push(<Page number={i}/>);
+    };
+
     return (
         <section className="pt-12">
             <div className="max-w-7xl mx-auto px-5 py-6 lg:px-0 flex gap-2 justify-end">
-                <div className="bg-blue-600 text-white px-4 py-1 rounded-full">
-                    1
-                </div>
-                <div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full">
-                    2
-                </div>
-                <div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full">
-                    3
-                </div>
-                <div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full">
+                {content}
+
+                {/* <div className="bg-blue-100 text-blue-600 px-4 py-1 rounded-full">
                     4
-                </div>
+                </div> */}
             </div>
         </section>
     );
