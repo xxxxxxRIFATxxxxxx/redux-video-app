866897ef059ad775902b20bacda2db4e9545f41e
Md. Rifat Islam <xxxxrifatxxxx@gmail.com>
2022-08-28 09:14:39 +0600

    Update API

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:9000",
+    baseURL: "https://thawing-ocean-45235.herokuapp.com/",
 });
 
 export default axiosInstance;
